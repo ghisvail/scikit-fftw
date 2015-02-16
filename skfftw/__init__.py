26b163ea5368df4a376e929c83920f526b0688bd
Ghislain Antony Vaillant <ghisvail@gmail.com>
2015-02-16 13:14:26 +0000

    make skfftw a package

---
+++ skfftw/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf8
+
+# Copyright (c) 2014, 2015 Ghislain Antony Vaillant.
+#
+# This file is distributed under the new BSD License, see the LICENSE file or 
+# checkout the license terms at http://opensource.org/licenses/BSD-3-Clause).
+
+from __future__ import absolute_import, division, print_function
