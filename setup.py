8efd5ce48de347fdbc327b1fb4ac6bd057f5153b
Ghislain Antony Vaillant <ghisvail@gmail.com>
2015-02-19 13:34:44 +0000

    handle setup commands with side effects

--- setup.py
+++ setup.py
@@ -14,9 +14,52 @@ from distutils.command.clean import clean
 from setuptools import setup, find_packages
 import os
 import shutil
+import sys
 
 
 packages = find_packages(exclude=['docs', 'tests'])
+
+
+SETUP_REQUIRES_ERROR = (
+    "Requested setup command that needs 'setup_requires' while command line "
+    "arguments implied a side effect free command or option."
+)
+
+NO_SETUP_REQUIRES_ARGUMENTS = [
+    "-h", "--help",
+    "-n", "--dry-run",
+    "-q", "--quiet",
+    "-v", "--verbose",
+    "-v", "--version",
+    "--author",
+    "--author-email",
+    "--classifiers",
+    "--contact",
+    "--contact-email",
+    "--description",
+    "--egg-base",
+    "--fullname",
+    "--help-commands",
+    "--keywords",
+    "--licence",
+    "--license",
+    "--long-description",
+    "--maintainer",
+    "--maintainer-email",
+    "--name",
+    "--no-user-cfg",
+    "--obsoletes",
+    "--platforms",
+    "--provides",
+    "--requires",
+    "--url",
+    "clean",
+    "egg_info",
+    "register",
+    "sdist",
+    "upload",
+]
+
 
 # Access to cffi extension
 def get_ext_modules():
@@ -30,12 +73,20 @@ class BuildCommand(build):
         self.distribution.ext_modules = get_ext_modules()
         build.finalize_options(self)
 
+class DummyBuildCommand(build):
+    def run(self):
+        raise RuntimeError(SETUP_REQUIRES_ERROR)
 
 # Intall command
 class InstallCommand(install):
     def finalize_options(self):
         self.distribution.ext_modules = get_ext_modules()
         install.finalize_options(self)
+
+class DummyInstallCommand(install):
+    def run(self):
+        raise RuntimeError(SETUP_REQUIRES_ERROR)
+
 
 # Clean command
 # adapted from blaze/setup.py
@@ -83,6 +134,55 @@ class CleanCommand(clean):
                 pass
 
 
+def keywords_with_side_effects(argv):
+    def is_short_option(argument):
+        """Check whether a command line argument is a short option."""
+        return len(argument) >= 2 and argument[0] == '-' and argument[1] != '-'
+
+    def expand_short_options(argument):
+        """Expand combined short options into canonical short options."""
+        return ('-' + char for char in argument[1:])
+
+    def argument_without_setup_requirements(argv, i):
+        """Check whether a command line argument needs setup requirements."""
+        if argv[i] in NO_SETUP_REQUIRES_ARGUMENTS:
+            # Simple case: An argument which is either an option or a command
+            # which doesn't need setup requirements.
+            return True
+        elif (is_short_option(argv[i]) and
+              all(option in NO_SETUP_REQUIRES_ARGUMENTS
+                  for option in expand_short_options(argv[i]))):
+            # Not so simple case: Combined short options none of which need
+            # setup requirements.
+            return True
+        elif argv[i - 1:i] == ['--egg-base']:
+            # Tricky case: --egg-info takes an argument which should not make
+            # us use setup_requires (defeating the purpose of this code).
+            return True
+        else:
+            return False
+
+    if all(argument_without_setup_requirements(argv, i)
+           for i in range(1, len(argv))):
+        return {
+            "cmdclass": {
+                "build": DummyBuildCommand,
+                "install": DummyInstallCommand,
+                "clean": CleanCommand,
+            }
+        }
+    else:
+        return {
+            "setup_requires": ["cffi"],
+            "cmdclass": {
+                "build": BuildCommand,
+                "install": InstallCommand,
+                "clean": CleanCommand,
+            },
+            "ext_package": "skfftw",
+        }
+
+
 with open(os.path.join("README.md")) as f:
     long_description = f.read()
 
@@ -118,12 +218,7 @@ setup(
     install_requires= [
         "cffi >= 0.6",
         ],
-    cmdclass={
-        "build": BuildCommand,
-        "install": InstallCommand,
-        "clean": CleanCommand,
-        },
     # for cffi
     zip_safe=False, 
-    ext_package="skfftw",   
+    **keywords_with_side_effects(sys.argv)   
     )
